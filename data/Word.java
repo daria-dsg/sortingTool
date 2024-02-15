b9007586586a900faa9bf0753c4c45c653ae694e
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-02-15 21:15:52 +0100

    refactor class

--- data/Word.java
+++ data/Word.java
@@ -2,28 +2,33 @@ package sorting.data;
 import java.util.*;
 
 public class Word extends DataType {
-    private final List<String> elements = new ArrayList<>();
+    private final List<String> words = new ArrayList<>();
     private String longestWord;
     private int frequency;
     void read() {
-        Scanner scanner = new Scanner(System.in);
         String word;
+        System.out.println("Enter words to proceed (press q to quit)");
 
         while (scanner.hasNext()) {
-            word = scanner.next();
-            elements.add(word);
+            String input = scanner.next();
+
+            if (input.equalsIgnoreCase("q")) {
+                break;
+            } else {
+                words.add(input);
+            }
         }
     }
 
     @Override
    void compute() {
-        longestWord = Collections.max(elements, Comparator.comparing(String::length));
-        frequency = Collections.frequency(elements, longestWord);
+        longestWord = Collections.max(words, Comparator.comparing(String::length));
+        frequency = Collections.frequency(words, longestWord);
     }
 
     @Override
     void print() {
-        System.out.printf("Total word:: %d.%n", elements.size());
-        System.out.printf("The longest word: %s (%d time(s), %d).%n", longestWord, frequency, (100 * frequency)/ elements.size());
+        System.out.printf("Total word:: %d.%n", words.size());
+        System.out.printf("The longest word: %s (%d time(s), %d%%).%n", longestWord, frequency, (100 * frequency)/ words.size());
     }
 }
