a2450ccab7ae1058f81110c1ab7cd5e179007356
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-02-25 21:57:34 +0100

    add exception to handle unknonw argument

--- ParseArgs.java
+++ ParseArgs.java
@@ -2,17 +2,28 @@ package sorting;
 
 import sorting.data.*;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class ParseArgs {
 
     private static String sorter;
     private static String dataType;
 
+    private static Set<String> knownArguments = new HashSet<>(Arrays.asList(
+            "-sortingType", "-dataType", "line", "word", "integer", "long", "natural", "byCount"
+    ));
+
     public static void parseArgs(String[] args) {
+        checkUnknownArguments(args);
+
         for (int i = 0; i < args.length; i++ ) {
-            if ("-sortingType".equals(args[i])) {
+            String argument = args[i];
+            if ("-sortingType".equals(argument)) {
                 checkSorterType(args[i+1]);
                 sorter = args[i + 1];
-            } else if ("-dataType".equals(args[i])) {
+            } else if ("-dataType".equals(argument)) {
                 checkDataType(args[i]);
                 dataType = args[i+1];
             }
@@ -42,6 +53,14 @@ public class ParseArgs {
     private static void checkSorterType(String str) {
         if(!str.equals("natural") && !str.equals("byCount") ) {
             throw new IllegalStateException("No sorting type defined!");
+        }
+    }
+
+    private static void checkUnknownArguments(String[] args) {
+        for (int i = 0; i < args.length; i++ ) {
+            if (!knownArguments.contains(args[i])) {
+                throw new IllegalArgumentException("\"" + args[i] + "\" is not a valid parameter. It will be skipped.");
+            }
         }
     }
 }
