026a163085d5960dbb09153d32a692daf3abb452
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-02-25 17:38:20 +0100

    rename sort algorithm

--- data/Line.java
+++ data/Line.java
@@ -1,49 +1,78 @@
-//package sorting.data;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Collections;
-//import java.util.Comparator;
-//
-//public class Line implements Sorter {
-//    private String longestLine;
-//    private final List<String> lines = new ArrayList<>();
-//    private int frequency;
-//
-//    @Override
-//    public void naturalSort() {
-//        read();
-//        Collections.sort(lines);
-//        printLines();
-//    }
-//
-//    private void read() {
-//        String input;
-//
-//        System.out.println("Enter lines to proceed (press q to quit)");
-//
-//        while (scanner.hasNextLine()) {
-//            input = scanner.nextLine();
-//            if (input.equalsIgnoreCase("q")) {
-//                break;
-//            } else {
-//                lines.add(input);
-//            }
-//        }
-//
-//        scanner.close();
-//    }
-//
-//
-//    void compute() {
-//        longestLine = Collections.max(lines, Comparator.comparing(String::length));
-//        frequency = Collections.frequency(lines, longestLine);
-//    }
-//
-//    private void printLines() {
-//        System.out.printf("Total lines: %d.%n", lines.size());
-//        System.out.println("The longest line: ");
-//        System.out.println(longestLine);
-//        System.out.printf("(%d time(s), %d%%).%n ", frequency, (100 * frequency)/ list.size() );
-//    }
-//}
+package sorting.data;
+
+import java.util.*;
+
+public class Line implements Sorter {
+    private String longestLine;
+    private final List<String> lines = new ArrayList<>();
+    private int frequency;
+
+    private final LinkedHashMap<String, Integer>  countMap= new LinkedHashMap<>();
+
+    @Override
+    public void naturalSort() {
+        read();
+        Collections.sort(lines);
+        printLines();
+    }
+
+    @Override
+    public void sortByCount() {
+        read();
+        sortingByCount();
+    }
+
+    private void read() {
+        String input;
+
+        System.out.println("Enter lines to proceed (press q to quit)");
+
+        while (scanner.hasNextLine()) {
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("q")) {
+                break;
+            } else {
+                lines.add(input);
+            }
+        }
+
+        scanner.close();
+    }
+
+    private void sortingByCount() {
+        for (String line : lines) {
+            countMap.put(line, countMap.getOrDefault(line, 0) + 1);
+        }
+
+        Comparator<Map.Entry<String, Integer>> sortByCountComparator = (entry1, entry2) -> {
+            int count1 = entry1.getValue();
+            int count2 = entry2.getValue();
+
+            if (count1 == count2) {
+                return 0;
+            }
+
+            return count1 - count2;
+        };
+
+        List<Map.Entry<String,Integer>> linesFromMap = new ArrayList<>(countMap.entrySet());
+        Collections.sort(linesFromMap, sortByCountComparator);
+
+        countMap.clear();
+        for (Map.Entry<String,Integer> entry : linesFromMap) {
+            countMap.put(entry.getKey(), entry.getValue());
+        }
+    }
+
+    private void compute() {
+        longestLine = Collections.max(lines, Comparator.comparing(String::length));
+        frequency = Collections.frequency(lines, longestLine);
+    }
+
+    private void printLines() {
+        System.out.printf("Total lines: %d.%n", lines.size());
+        System.out.println("The longest line: ");
+        System.out.println(longestLine);
+        System.out.printf("(%d time(s), %d%%).%n ", frequency, (100 * frequency)/ lines.size() );
+    }
+}
