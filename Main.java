4bb951cd1df7a90c36a25c5fc8e771fb1fc7153c
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-02-25 21:28:57 +0100

    add error exceptions for not provided arguments

--- Main.java
+++ Main.java
@@ -2,16 +2,23 @@ package sorting;
 
 import sorting.data.*;
 
-import java.util.*;
+import java.lang.reflect.Parameter;
 
 public class Main {
     public static void main(final String[] args) {
-        DataType dataToSort = ParseArgs.parseArgs(args);
+        ParseArgs.parseArgs(args);
+
+        Sorter dataToSort = ParseArgs.getDataType();
         String sortType = ParseArgs.getSorter();
 
         switch(sortType) {
-            case "natural": dataToSort.naturalSort;
-            case "byCount": dataToSort.sortByCount;
+            case "natural":
+                dataToSort.naturalSort();
+                break;
+            case "byCount":
+                dataToSort.sortByCount();
+                break;
+            default: throw new IllegalArgumentException("No sorting type defined!");
         }
     }
 }
--- ParseArgs.java
+++ ParseArgs.java
@@ -4,40 +4,30 @@ import sorting.data.*;
 
 public class ParseArgs {
 
-    private static String sorter = "natural";
+    private static String sorter;
+    private static String dataType;
 
-    public static Sorter parseArgs(String[] args) {
-        String dataTypeInput = "";
-
+    public static void parseArgs(String[] args) {
         for (int i = 0; i < args.length; i++ ) {
             if ("-sortingType".equals(args[i])) {
-                checkSortingType(args[i+1]);
                 sorter = args[i + 1];
             } else if ("-dataType".equals(args[i])) {
-                dataTypeInput = args[i + 1];
+                dataType = args[i+1];
             }
         }
-
-        return parseDataType(dataTypeInput);
     }
 
     public static String getSorter() {
         return sorter;
     }
 
-    private static Sorter parseDataType(String input) {
-        return switch (input) {
+    public static Sorter getDataType() {
+        return switch (dataType) {
             case "long" -> new Longs();
             case "line" -> new Line();
             case "integer" -> new Integers();
             case "word" -> new Word();
             default -> throw new IllegalStateException("No data type defined!");
         };
-    }
-
-    private static void checkSortingType(String str) {
-        if (!str.equals("natural") || !str.equals("sortByCount")) {
-            throw new IllegalArgumentException("No sorting type defined!");
-        }
     }
 }
