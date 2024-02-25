a5a36248dcb0a1c1c953c285879c102d1e3029a8
Daria Zhdanova <94113384+daria-dsg@users.noreply.github.com>
2024-02-25 21:14:47 +0100

    add exception if arguments not complete

--- ParseArgs.java
+++ ParseArgs.java
@@ -11,6 +11,7 @@ public class ParseArgs {
 
         for (int i = 0; i < args.length; i++ ) {
             if ("-sortingType".equals(args[i])) {
+                checkSortingType(args[i+1]);
                 sorter = args[i + 1];
             } else if ("-dataType".equals(args[i])) {
                 dataTypeInput = args[i + 1];
@@ -29,7 +30,14 @@ public class ParseArgs {
             case "long" -> new Longs();
             case "line" -> new Line();
             case "integer" -> new Integers();
-            default -> new Word();
+            case "word" -> new Word();
+            default -> throw new IllegalStateException("No data type defined!");
         };
+    }
+
+    private static void checkSortingType(String str) {
+        if (!str.equals("natural") || !str.equals("sortByCount")) {
+            throw new IllegalArgumentException("No sorting type defined!");
+        }
     }
 }
